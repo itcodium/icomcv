41343c737f8a709673131d53e3dfbbd8029550a6
itcodium <it.codium@gmail.com>
2015-10-30 19:22:40 -0200

    'comentario'

--- rotate/perspective.py
+++ rotate/perspective.py
@@ -11,6 +11,7 @@ print "img.shape",img.shape
 
 M = cv2.getRotationMatrix2D((cols/2,rows/2),-0.25,1)
 dst = cv2.warpAffine(img,M,(cols,rows))
+cv2.imwrite("rotate.jpg", dst)
 	
 plt.subplot(121),plt.imshow(img),plt.title('Input')
 plt.subplot(122),plt.imshow(dst),plt.title('Output')
